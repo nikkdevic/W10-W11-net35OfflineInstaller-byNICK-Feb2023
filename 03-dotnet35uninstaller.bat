93f6049bcb8f77d5944ff61a3c49d558ab147b87
nikkdevic <97282126+nikkdevic@users.noreply.github.com>
2023-02-02 12:09:21 +0530

    Update 03-dotnet35uninstaller.bat

--- 03-dotnet35uninstaller.bat
+++ 03-dotnet35uninstaller.bat
@@ -4,7 +4,7 @@ if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
 :: ----------------------------------------------------------------------------------
 
-:: Check for Win 10 Only =======================================================
+:: Check for Win 10 and Above =======================================================
 @echo off
 if exist "%Windir%\Sysnative\reg.exe" (set "SysPath=%Windir%\Sysnative") else (set "SysPath=%Windir%\System32")
 set "Path=%SysPath%;%Windir%;%SysPath%\Wbem"
@@ -12,7 +12,7 @@ for /f "tokens=6 delims=[]. " %%G in ('ver') do set winbuild=%%G
 if %winbuild% LSS 10240 (
   if not "%1"=="" goto :eof
   echo ==== ERROR ====
-  echo This package is only for Windows 10
+  echo This package is only for Windows 10 and Above
   echo Press any Key to Exit.
   pause >nul
   goto :eof
